ceed0f99a762adc5cc0dccaf44e37c3f68e2b2b7
SwellPretense <151082164+SwellPretense@users.noreply.github.com>
2024-11-21 16:33:58 -0500

    Add project files.

---
+++ Gun.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roull
+{
+    public class Gun
+    {
+        // Save Array Of Bullets With Minimal Data
+        public Gun(int live, int neutral) { }
+
+        public void Fire()
+        {
+            // Use Loaded Bullet
+        }
+    }
+}
